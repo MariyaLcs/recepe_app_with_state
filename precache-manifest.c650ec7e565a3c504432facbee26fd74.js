637098d9f03bf17938e9fed50abd3da77a272a16
Mariya <mariya.podosinova@gmail.com>
2020-05-29 20:50:10 +0100

    Updates

---
+++ precache-manifest.c650ec7e565a3c504432facbee26fd74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66813dc5a02a4cb40d2104f82ceb5b2",
+    "url": "/recepe_app_with_state/index.html"
+  },
+  {
+    "revision": "4cd8b638dcad77364f04",
+    "url": "/recepe_app_with_state/static/css/main.843b4e9f.chunk.css"
+  },
+  {
+    "revision": "1888fbc2bb0a4a6179fd",
+    "url": "/recepe_app_with_state/static/js/2.a62ee947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recepe_app_with_state/static/js/2.a62ee947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd8b638dcad77364f04",
+    "url": "/recepe_app_with_state/static/js/main.d0265f4e.chunk.js"
+  },
+  {
+    "revision": "92889a449a4502de1491",
+    "url": "/recepe_app_with_state/static/js/runtime-main.d6a8e514.js"
+  }
+]);
